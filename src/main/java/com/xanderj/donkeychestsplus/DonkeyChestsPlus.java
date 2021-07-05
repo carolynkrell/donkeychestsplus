04cbf23ba4dfa63c35a3bdb48c01b5309e4744ba
BuildTools <xkrell10@gmail.com>
2021-07-04 20:49:55 -0400

    initial commit (with actual code)

---
+++ src/main/java/com/xanderj/donkeychestsplus/DonkeyChestsPlus.java
@@ -0,0 +1,9 @@
+package com.xanderj.donkeychestsplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DonkeyChestsPlus implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
